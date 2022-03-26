1f6d7cc1d7c6ddb237b1959cda5d8d5a597e44d9
Niccolò Laurenti <niclaurenti@gmail.com>
2022-03-26 19:17:17 +0100

    Clean HighScaleCoefficientFunctions

--- include/HighScaleCoefficientFunctions.h
+++ include/HighScaleCoefficientFunctions.h
@@ -13,33 +13,31 @@ double DLm_g1_highscale(double x, double mQ);
 
 //NNLO => O(\alpha_s^2)
 double C2m_g2_highscale(double x, double mQ, double mMu);
-double CLm_g2_highscale(double x, double mQ, double mMu);
-
 double C2m_ps2_highscale(double x, double mQ, double mMu);
+
+double CLm_g2_highscale(double x, double mQ, double mMu);
 double CLm_ps2_highscale(double x, double mQ, double mMu);
 
 double D2m_g2_highscale(double x, double mQ, double mMu);
-double DLm_g2_highscale(double x, double mQ, double mMu);
-
 double D2m_ps2_highscale(double x, double mQ, double mMu);
-double DLm_ps2_highscale(double x, double mQ, double mMu);
 
+double DLm_g2_highscale(double x, double mQ, double mMu);
+double DLm_ps2_highscale(double x, double mQ, double mMu);
 
 //N3LO => O(\alpha_s^3)
 double C2m_g3_highscale(double x, double mQ, double mMu, int nf, int v);
-double D2m_g3_highscale(double x, double mQ, double mMu, int nf, int v);
-
-double D2m_ps3_highscale(double x, double mQ, double mMu, int nf);
 double C2m_ps3_highscale(double x, double mQ, double mMu, int nf);
 
 double CLm_g3_highscale(double x, double mQ, double mMu, int nf);
-
 double CLm_ps3_highscale(double x, double mQ, double mMu, int nf);
+
+double D2m_g3_highscale(double x, double mQ, double mMu, int nf, int v);
+double D2m_ps3_highscale(double x, double mQ, double mMu, int nf);
 
 
 //////////////////////////////////////
-double D2m_ps3_highscaleVogt(double x, double mQ, double mMu, int nf, int v);
 double C2m_ps3_highscaleVogt(double x, double mQ, double mMu, int nf, int v);
+double D2m_ps3_highscaleVogt(double x, double mQ, double mMu, int nf, int v);
 
 double C2m_ps2_highscaleNEW(double x, double mQ, double mMu);
 
