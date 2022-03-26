7f731a999a6f52dfc246610561b8c9bc266082f7
Niccolò Laurenti <niclaurenti@gmail.com>
2022-03-26 17:22:17 +0100

    Order AsymptoticCoefficientFunctions

--- include/AsymptoticCoefficientFunctions.h
+++ include/AsymptoticCoefficientFunctions.h
@@ -1,20 +1,28 @@
 #ifndef Asymptotic_h
 #define Asymptotic_h
 
+//asymptotic coefficient funtions O(alpha_s)
+
 double C2m_g1_asymptotic(double x, double mQ);
+double CLm_g1_asymptotic(double x, double mQ);
+
+//asymptotic coefficient funtions O(alpha_s^2)
+
 double C2m_g2_asymptotic(double x, double mQ, double mMu);
 double C2m_ps2_asymptotic(double x, double mQ, double mMu);
-//double C2m_g2_asymptoticV2(double x, double mQ, double mMu);
 
-double CLm_g1_asymptotic(double x, double mQ);
 double CLm_g2_asymptotic(double x, double mQ, double mMu);
 double CLm_ps2_asymptotic(double x, double mQ, double mMu);
-//double CLm_g2_asymptoticV2(double x, double mQ, double mMu);
+
+//asymptotic coefficient funtions O(alpha_s^3)
 
 double C2m_g3_asymptoticLL(double x, double mQ, double mMu, int nf, int v);
 double C2m_g3_asymptoticNLL(double x, double mQ, double mMu, int nf, int v1=0, int v2=0);
+
 double C2m_ps3_asymptoticNLL(double x, double mQ, double mMu, int nf);
+
 double CLm_g3_asymptoticNLL(double x, double mQ, double mMu, int nf);
+
 double CLm_ps3_asymptoticNLL(double x, double mQ, double mMu, int nf);
 
 #endif
