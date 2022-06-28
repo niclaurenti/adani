60707c7349ff02552665c979a1dc36f0a264836f
Niccolò Laurenti <niclaurenti@gmail.com>
2022-06-28 16:08:07 +0200

    Clean Colorfactors

--- include/ColorFactors.h
+++ include/ColorFactors.h
@@ -1,8 +1,8 @@
 #ifndef Color_h
 #define Color_h
 
-const double TR	=	1./2.;
-const double CF	=	4./3.;
-const double CA	=	3.;
+const double TR	= 1. / 2. ;
+const double CF	= 4. / 3. ;
+const double CA	= 3. ;
 
 #endif
