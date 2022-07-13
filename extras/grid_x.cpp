343f759b9e5dc35853c0a513b28d0a22f58c2855
Niccolò Laurenti <niclaurenti@gmail.com>
2022-07-13 17:57:14 +0200

    Remove tabs from grid_x.cpp

--- extras/grid_x.cpp
+++ extras/grid_x.cpp
@@ -7,28 +7,28 @@ using namespace std;
 int main(int argc, char** argv) {
 
     if(argc!=4) {
-		cout << "ERROR\nUsage: ./grid_x.exe logx_min N_points filename\nExiting..." << endl;
-		return -1;
-	}
+        cout << "ERROR\nUsage: ./grid_x.exe logx_min N_points filename\nExiting..." << endl;
+        return -1;
+    }
 
     double logx_min = atof(argv[1]);
     int N_points = atoi(argv[2]) ;
  
-	ofstream output;
-	output.open(argv[3]);
+    ofstream output;
+    output.open(argv[3]);
 
-    double x, logx, logx_max=0;	
-	
-	double dlog=(logx_max - logx_min)/N_points;
+    double x, logx, logx_max=0;    
+    
+    double dlog=(logx_max - logx_min)/N_points;
 
     for(int i=0; i <= N_points; i++) {
-		
-		logx=logx_min + i*dlog;
-		
-		x=pow(10, logx);
+        
+        logx=logx_min + i*dlog;
+        
+        x=pow(10, logx);
 
         output  << x << "   " ;
-	}
+    }
 
     output << endl ;
 
