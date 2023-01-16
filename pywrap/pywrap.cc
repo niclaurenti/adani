70dc2ced4be5dd27aa478aa5832e0bd2077c95b6
Niccolò Laurenti <niclaurenti@gmail.com>
2023-01-16 22:22:01 +0100

    Remove string from pywrap

--- pywrap/pywrap.cc
+++ pywrap/pywrap.cc
@@ -11,16 +11,16 @@ PYBIND11_MODULE(adanipy, m) {
     // Documentation
     m.doc() = "Python wrapper of libadani";
 
-    m.def("C2m_g3_approximation", &C2m_g3_approximation, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"), py::arg("method_flag")=1, py::arg("calls")=500000);
-    m.def("CLm_g3_approximation", &CLm_g3_approximation, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"), py::arg("method_flag")=1, py::arg("calls")=500000);
-    m.def("C2m_ps3_approximation", &C2m_ps3_approximation, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
-    m.def("CLm_ps3_approximation", &CLm_ps3_approximation, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_g3_approximation", &C2m_g3_approximation, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"), py::arg("method_flag")=1, py::arg("calls")=500000);
+    m.def("CLm_g3_approximation", &CLm_g3_approximation, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"), py::arg("method_flag")=1, py::arg("calls")=500000);
+    m.def("C2m_ps3_approximation", &C2m_ps3_approximation, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("CLm_ps3_approximation", &CLm_ps3_approximation, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
 
-    m.def("C2m_g3_approximationA_klmv", &C2m_g3_approximationA_klmv, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
-    m.def("C2m_g3_approximationB_klmv", &C2m_g3_approximationB_klmv, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
-    m.def("C2m_g3_approximationBlowxi_klmv", &C2m_g3_approximationBlowxi_klmv, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_g3_approximationA_klmv", &C2m_g3_approximationA_klmv, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_g3_approximationB_klmv", &C2m_g3_approximationB_klmv, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_g3_approximationBlowxi_klmv", &C2m_g3_approximationBlowxi_klmv, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
 
-    m.def("C2m_ps3_approximationA_klmv", &C2m_ps3_approximationA_klmv, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
-    m.def("C2m_ps3_approximationB_klmv", &C2m_ps3_approximationB_klmv, "", py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_ps3_approximationA_klmv", &C2m_ps3_approximationA_klmv, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
+    m.def("C2m_ps3_approximationB_klmv", &C2m_ps3_approximationB_klmv, py::arg("x"), py::arg("mQ"), py::arg("mMu"), py::arg("nf"));
 
 }
