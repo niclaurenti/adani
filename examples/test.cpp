620bafe0fd0dd5738521404b2048662bd16cd197
Niccolò Laurenti <niclaurenti@gmail.com>
2023-01-16 21:47:04 +0100

    Fix scope

--- examples/test.cpp
+++ examples/test.cpp
@@ -10,9 +10,9 @@ int main() {
 
     double x, logx, logx_min=-4, logx_max=0;
     double Q, logQ, logQ_min=1, logQ_max=2;
-	int N=5;
+    int N=5;
 
-	double dlogx=(logx_max - logx_min) / N;
+    double dlogx=(logx_max - logx_min) / N;
     double dlogQ=(logQ_max - logQ_min) / N;
 
     double m = 4.92, mQ, mMu ;
@@ -22,9 +22,9 @@ int main() {
         for(int j=0; j<N; j++) {
 
             logx = logx_min + i * dlogx;
-		    x = pow(10, logx);
+            x = pow(10, logx);
             logQ = logQ_min + j * dlogQ;
-		    Q = pow(10, logQ);
+            Q = pow(10, logQ);
 
             mQ = m * m / (Q * Q) ;
             mMu = mQ ;
