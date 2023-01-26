4c9f36a36468554229bf9361121419e3c2b98407
Niccolò Laurenti <niclaurenti@gmail.com>
2023-01-26 10:34:12 +0100

    Move stuff back

--- src/Convolutions.cc
+++ src/Convolutions.cc
@@ -5,10 +5,7 @@
 #include "adani/MatchingConditions.h"
 #include "adani/SpecialFunctions.h"
 #include "adani/SplittingFunctions.h"
-#include <gsl/gsl_integration.h>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_monte.h>
-#include <gsl/gsl_monte_vegas.h>
+
 #include <cmath>
 #include <iostream>
 
