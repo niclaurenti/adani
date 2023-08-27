84cfa31868f1d11596d59fcca042661ec6cbe5eb
Niccolò Laurenti <niclaurenti@gmail.com>
2023-08-27 16:08:40 +0200

    Add .cc and .h for HighScaleSplitLogs

---
+++ inc/adani/HighScaleSplitLogs.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  HighScaleSplitLogs.h
+ *
+ *    Description:  Header file for the
+ * HighScaleSplitLogs.cc file.
+ *
+ *         Author:  Find Quote
+ *
+ *  In this file there are the coefficient functions in the
+ *  high scale limit, i.e. Q^2 >> m^2, implemented setting mu=Q and splitting
+ *  the terms proportional to each logarithm
+ *
+ * =====================================================================================
+ */
+
+#ifndef HighScaleLogs_h
+#define HighScaleLogs_h
+
+
+
+#endif
---
+++ src/HighScaleSplitLogs.cc
@@ -0,0 +1,8 @@
+#include "adani/HighScaleSplitLogs.h"
+#include "adani/Constants.h"
+#include "adani/Convolutions.h"
+#include "adani/MasslessCoefficientFunctions.h"
+#include "adani/MatchingConditions.h"
+#include "adani/SpecialFunctions.h"
+#include <cmath>
+#include <iostream>
