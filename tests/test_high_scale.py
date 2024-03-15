865cb2324252ac2afd3c504ede846c99cea89e7a
Niccolò Laurenti <niclaurenti@gmail.com>
2024-03-15 11:48:13 +0100

    Add hs test

--- tests/test_high_scale.py
+++ tests/test_high_scale.py
@@ -1,7 +1,7 @@
 import adani as ad
 import numpy as np
 
-def test_split_logs():
+def test_splitlogs_vs_highscale():
     for kind in ['2', 'L']:
         for channel in ['g', 'q']:
             hs = ad.HighScaleCoefficientFunction(3, kind, channel)
@@ -12,5 +12,24 @@ def test_split_logs():
                     for nf in range(1, 6 + 1):
                         for v in [0]:
                             res1 = hs.fx(x, 1./xi, 1./xi, nf)
+                            res2 = hs_split.fx(x, 1./xi, nf, v)
+                            np.testing.assert_allclose(res1, res2, rtol = 1e-7)
+
+def test_split_logs():
+    for kind in ['2', 'L']:
+        for channel in ['g', 'q']:
+            hs_split = ad.HighScaleSplitLogs(3, kind, channel)
+            
+            for x in np.geomspace(1e-5, 1, 100, endpoint=False):
+                for xi in np.geomspace(1e-2, 1e4, 10, endpoint=True):
+                    for nf in range(1, 6 + 1):
+                        for v in [0]:
+                            logQ = np.log(1/xi)
+                            res1 = (
+                                hs_split.LL(x, nf) * logQ**3
+                                + hs_split.NLL(x, nf) * logQ**2
+                                + hs_split.N2LL(x, nf) * logQ
+                                + hs_split.N3LL(x, nf, v)
+                            )
                             res2 = hs_split.fx(x, 1./xi, nf, v)
                             np.testing.assert_allclose(res1, res2, rtol = 1e-7)
