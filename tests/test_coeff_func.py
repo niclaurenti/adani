dce912b6dc102665230410050b3d29938772b353
niccolo <niclaurenti@gmail.com>
2024-03-21 15:26:44 +0100

    Remove files

--- tests/test_coeff_func.py
+++
@@ -1,7 +0,0 @@
-import oldadani
-
-def test_C2_g3_approx():
-    m2Q2 = 1/10**2
-    nf=4
-    for x in [1e-4, 1e-3, 1e-2, 1e-1, 0.5]:
-        oldadani.C2_g3_approximation(x, m2Q2, m2Q2, nf, v=0, method_flag=1)
