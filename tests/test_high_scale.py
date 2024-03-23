06ab7d2ed75737dba6ec0fe9d98fe08dfbeddf16
Niccolò Laurenti <niclaurenti@gmail.com>
2024-03-23 17:55:21 +0100

    Test oldadani hs

--- tests/test_high_scale.py
+++ tests/test_high_scale.py
@@ -1,5 +1,55 @@
 import adani as ad
+import oldadani as old
 import numpy as np
+from math import nan
+
+def test_lo_oldadani():
+    for kind in ['2', 'L']:
+        hs = ad.HighScaleCoefficientFunction(1, kind, 'g')
+        for x in np.geomspace(1e-5, 1., 100, endpoint=False):
+            for m2Q2 in np.geomspace(1e-4, 1e-2, 10):
+                res1 = hs.fx(x, m2Q2, 0., 0)
+                res2 = old.C2_g1_highscale(x, m2Q2) if kind == '2' else old.CL_g1_highscale(x)
+                np.testing.assert_allclose(res1, res2, rtol=1e-7)
+
+def test_nnlo_oldadani():
+    for kind in ['2', 'L']:
+        for channel in ['g', 'q']:
+            hs = ad.HighScaleCoefficientFunction(2, kind, channel)
+            for x in np.geomspace(1e-5, 1., 100, endpoint=False):
+                for m2Q2 in np.geomspace(1e-4, 1e-2, 10):
+                    for m2mu2 in np.geomspace(1e-4, 1e-2, 10):
+                        res1 = hs.fx(x, m2Q2, m2mu2, 0)
+                        if (kind == '2' and channel == 'g'):
+                            res2 = old.C2_g2_highscale(x, m2Q2, m2mu2)
+                        if (kind == '2' and channel == 'q'):
+                            res2 = old.C2_ps2_highscale(x, m2Q2, m2mu2)
+                        if (kind == 'L' and channel == 'g'):
+                            res2 = old.CL_g2_highscale(x, m2Q2, m2mu2)
+                        if (kind == 'L' and channel == 'q'):
+                            res2 = old.CL_ps2_highscale(x, m2Q2, m2mu2)
+                        np.testing.assert_allclose(res1, res2, rtol=1e-7)
+
+
+def test_n3lo_oldadani():
+    for kind in ['2', 'L']:
+        for channel in ['g', 'q']:
+            exact = True if channel == 'q' else False
+            hs = ad.HighScaleCoefficientFunction(3, kind, channel, exact)
+            for x in np.geomspace(1e-5, 1., 100, endpoint=False):
+                for m2Q2 in np.geomspace(1e-4, 1e-2, 10):
+                    for m2mu2 in np.geomspace(1e-4, 1e-2, 10):
+                        for nf in range(6 + 1):
+                            res1 = hs.fx(x, m2Q2, m2mu2, nf)
+                            if (kind == '2' and channel == 'g'):
+                                res2 = old.C2_g3_highscale(x, m2Q2, m2mu2, nf)
+                            if (kind == '2' and channel == 'q'):
+                                res2 = old.C2_ps3_highscale(x, m2Q2, m2mu2, nf)
+                            if (kind == 'L' and channel == 'g'):
+                                res2 = old.CL_g3_highscale(x, m2Q2, m2mu2, nf)
+                            if (kind == 'L' and channel == 'q'):
+                                res2 = old.CL_ps3_highscale(x, m2Q2, m2mu2, nf)
+                            np.testing.assert_allclose(res1, res2, rtol=1e-7)
 
 def test_splitlogs_vs_highscale():
     for kind in ['2', 'L']:
