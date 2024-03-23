ab6745f2bb0b504374beea083bf9b29cf4353467
Niccolò Laurenti <niclaurenti@gmail.com>
2024-03-23 17:15:39 +0100

    Fix test thr

--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -164,11 +164,17 @@ def test_nlo():
                 xmax = 1. / (1 + 4 * m2Q2)
                 for x in np.geomspace(1e-5, xmax, 100, endpoint=False):
                     res1 = thr.fx(x, m2Q2, m2mu2, 1)
-                    res2 = old.C_g2_threshold(x, m2Q2, m2mu2, kind)
+                    if kind == '2':
+                        res2 = old.C2_g2_threshold(x, m2Q2, m2mu2)
+                    else:
+                        res2 = old.CL_g2_threshold(x, m2Q2, m2mu2)
                     np.testing.assert_allclose(res1, res2, rtol=1e-7)
 
                     res1 = thr.BetaIndependentTerms(x, m2Q2, m2mu2)
-                    res2 = old.C_g2_threshold_const(x, m2Q2, m2mu2, kind)
+                    if kind == '2':
+                        res2 = old.C2_g2_threshold_const(x, m2Q2, m2mu2)
+                    else:
+                        res2 = old.CL_g2_threshold_const(x, m2Q2, m2mu2)
                     np.testing.assert_allclose(res1, res2, rtol=1e-7)
 
 def test_nnlo():
@@ -180,9 +186,15 @@ def test_nnlo():
                 for x in np.geomspace(1e-5, xmax, 100, endpoint=False):
                     for nf in range(1, 6 + 1):
                         res1 = thr.fx(x, m2Q2, m2mu2, nf)
-                        res2 = old.C_g3_threshold(x, m2Q2, m2mu2, nf, kind)
+                        if kind == '2':
+                            res2 = old.C2_g3_threshold(x, m2Q2, m2mu2, nf)
+                        else:
+                            res2 = old.CL_g3_threshold(x, m2Q2, m2mu2, nf)
                         np.testing.assert_allclose(res1, res2, rtol=1e-7)
 
                         res1 = thr.BetaIndependentTerms(x, m2Q2, m2mu2)
-                        res2 = old.C_g3_threshold_const(x, m2Q2, m2mu2, kind)
+                        if kind == '2':
+                            res2 = old.C2_g3_threshold_const(x, m2Q2, m2mu2, nf)
+                        else:
+                            res2 = old.CL_g3_threshold_const(x, m2Q2, m2mu2, nf)
                         np.testing.assert_allclose(res1, res2, rtol=1e-7)
