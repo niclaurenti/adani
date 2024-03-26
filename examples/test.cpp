27fe37aee0fb7385a9d37d4e9795f31d44bf6c3b
niccolo <niclaurenti@gmail.com>
2024-03-26 14:16:37 +0100

    Fix exaples

--- examples/test.cpp
+++ examples/test.cpp
@@ -18,6 +18,11 @@ int main() {
     double m = 4.92, m2Q2, m2mu2 ;
     int nf = 4 ;
 
+    ApproximateCoefficientFunction F2g(3, '2', 'g', true, false, true, 1e-3, 1e-3, 1000, 0, 25000);
+    ApproximateCoefficientFunction FLg(3, 'L', 'g', true, false, true, 1e-3, 1e-3, 1000, 0, 25000);
+    ApproximateCoefficientFunction F2q(3, '2', 'q', true, false, true, 1e-3, 1e-3, 1000, 0, 25000);
+    ApproximateCoefficientFunction FLq(3, 'L', 'q', true, false, true, 1e-3, 1e-3, 1000, 0, 25000);
+
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
 
@@ -29,10 +34,10 @@ int main() {
             m2Q2 = m * m / (Q * Q) ;
             m2mu2 = m2Q2 ;
 
-            cout << "C2_g^(3)(x=" << x << ", Q="<< Q << ") = "<< C2_g3_approximation(x, m2Q2, m2mu2, nf) << endl ;
-            cout << "C2_ps^(3)(x=" << x << ", Q="<< Q << ") = "<< C2_ps3_approximation(x, m2Q2, m2mu2, nf) << endl ;
-            cout << "CL_g^(3)(x=" << x << ", Q="<< Q << ") = "<< CL_g3_approximation(x, m2Q2, m2mu2, nf) << endl ;
-            cout << "CL_ps^(3)(x=" << x << ", Q="<< Q << ") = "<< CL_ps3_approximation(x, m2Q2, m2mu2, nf) << endl ;
+            cout << "C2_g^(3)(x=" << x << ", Q="<< Q << ") = "<< F2g.fxBand(x, m2Q2, m2mu2, nf) << endl ;
+            cout << "C2_ps^(3)(x=" << x << ", Q="<< Q << ") = "<< F2q.fxBand(x, m2Q2, m2mu2, nf) << endl ;
+            cout << "CL_g^(3)(x=" << x << ", Q="<< Q << ") = "<< FLg.fxBand(x, m2Q2, m2mu2, nf) << endl ;
+            cout << "CL_ps^(3)(x=" << x << ", Q="<< Q << ") = "<< FLq.fxBand(x, m2Q2, m2mu2, nf) << endl ;
         }
     }
     return 0;
--- examples/test.py
+++ examples/test.py
@@ -1,14 +1,19 @@
-import adani
+import adani as ad
 import numpy as np
 
 nf = 4
 m = 4.92
 
+F2g = ad.ApproximateCoefficientFunction(3, '2', 'g', True, False, True, 1e-3, 1e-3, 1000, 0, 25000)
+FLg = ad.ApproximateCoefficientFunction(3, 'L', 'g', True, False, True, 1e-3, 1e-3, 1000, 0, 25000)
+F2q = ad.ApproximateCoefficientFunction(3, '2', 'q', True, False, True, 1e-3, 1e-3, 1000, 0, 25000)
+FLq = ad.ApproximateCoefficientFunction(3, 'L', 'q', True, False, True, 1e-3, 1e-3, 1000, 0, 25000)
+
 for x in np.geomspace(1e-4, 1, 5):
     for Q in np.geomspace(10, 100, 5):
         m2Q2 = m**2 / Q**2
         m2mu2 = m2Q2
-        print("C2_g^(3)(x=", x, ", Q=", Q, ") = ", adani.C2_g3_approximation(x, m2Q2, m2mu2, nf))
-        print("C2_ps^(3)(x=", x, ", Q=", Q, ") = ", adani.C2_ps3_approximation(x, m2Q2, m2mu2, nf))
-        print("CL_g^(3)(x=", x, ", Q=", Q, ") = ", adani.CL_g3_approximation(x, m2Q2, m2mu2, nf))
-        print("CL_ps^(3)(x=", x, ", Q=", Q, ") = ", adani.CL_ps3_approximation(x, m2Q2, m2mu2, nf))
+        print("C2_g^(3)(x=", x, ", Q=", Q, ") = ", F2g.fx(x, m2Q2, m2mu2, nf))
+        print("C2_ps^(3)(x=", x, ", Q=", Q, ") = ", F2g.fx(x, m2Q2, m2mu2, nf))
+        print("CL_g^(3)(x=", x, ", Q=", Q, ") = ", F2g.fx(x, m2Q2, m2mu2, nf))
+        print("CL_ps^(3)(x=", x, ", Q=", Q, ") = ", F2g.fx(x, m2Q2, m2mu2, nf))
